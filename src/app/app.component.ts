060afe9bbb1124dfa8295b5d532077853d1d905c
Tiago Correia Ribeiro <tiago.cr@aluno.ifsc.edu.br>
2019-12-05 09:07:20 -0300

    Ajustes na criação das vagas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  vagas = [];
+
+  constructor() {
+    
+    for (let index = 0; index < 108; index++) {
+      this.vagas[index] = index;
+    }
+  }
   title = 'frontend-ws';
 }
 
