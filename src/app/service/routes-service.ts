10dc2415e3d5266cfca6cc91b599aeb2ce0ef3e9
Tiago Correia Ribeiro <tiago.cr@aluno.ifsc.edu.br>
2019-12-12 08:43:30 -0300

    Ajustes no link da api de Rotas

--- src/app/service/routes-service.ts
+++ src/app/service/routes-service.ts
@@ -5,17 +5,13 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class RoutesService {
 
-  private readonly API = 'https://back-end-ifscmap.herokuapp.com/api/';
+  private readonly API = 'https://ifscrouting.herokuapp.com/api/';
 
   constructor(private http: HttpClient) { }
 
 
   getCaminho(local: string) {
     return this.http.post(`${this.API}caminho`, local);
-  }
-
-  getCaminho2(): Observable<string> {
-    return this.http.get<string>(`${this.API}exemplo`);
   }
 
 }
